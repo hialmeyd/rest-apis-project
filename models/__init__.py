f0182feed305c1d0112d17600098ebe0e836579d
hialmeyd <hialmeyd_tmp@hotmail.com>
2025-02-25 14:06:22 -0600

    First commit!

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+# Se requiere para que las tablas sean creadas por SQLAlchemy
+from models.store import StoreModel
+from models.item import ItemModel
+from models.tag import TagModel
+from models.item_tags import ItemTags
+from models.user import UserModel
